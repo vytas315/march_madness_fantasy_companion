9f980a30870a3ab124e31551af695fa7109ddb4e
Vytas Bradunas <vb@refunite.org>
2016-02-20 17:04:19 +0300

    Basic site working

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App changed</h1>'
+})
+export class AppComponent { }
